a741194fe54e117366d3fd08a208ce38df6c835c
David Solá <david@adsmurai.com>
2017-04-30 11:50:21 +0200

    Add first version of Script R with preprocessing

---
+++ PCA.R
@@ -0,0 +1,54 @@
+library(FactoMineR);
+
+## Load the dataset
+appears <- read.csv("dataset/300k.csv",header=T)
+## Display the columns
+str(appears)
+appearColNames <- colnames(appears)
+
+## Check different years (ignore if it's always the same)
+unique(appears$appearedYear) # Only one value, so we can delete from dataset
+appears$appearedYear <- NULL
+
+## Check different months (ignore if it's always the same)
+unique(appears$appearedMonth) # Only one value, so we can delete from dataset
+appears$appearedMonth <- NULL
+
+## Join (urban, suburban, midurban and rural) to only one factor variable
+
+appears[1:100,c("urban","suburban","midurban","rural")] # Not sure to transform it, because a row can be multiple values
+unique(appears[,c("urban","suburban","midurban","rural")]) # Check the posible combinations
+
+## Delete gymInXm because it's informed in gymDistanceKm
+gymInMatches <- subset(appearColNames, grepl("gymIn", appearColNames))
+appears[, gymInMatches] <- NULL
+
+## Delete pokestopInX because it's informed in pokestopDistanceKm
+pokestopInMatches <- subset(appearColNames, grepl("pokestopIn", appearColNames))
+appears[, pokestopInMatches] <- NULL
+
+## Delete co-occurrence because it's not necessary to generate a PCA
+coocMatches <- subset(appearColNames, grepl("cooc_", appearColNames))
+appears[, coocMatches] <- NULL
+
+urbanColNames <- c("rural","midurban","suburban","urban")
+## Urban -> urban + suburban + midurban
+## Suburban -> suburban + midurban
+## Rural <- rural
+## MidUrban <- midurban
+
+## This function returns the new category variable following the previous rules
+defineUrban <- function(appear){
+  if (appear["rural"] == "true") { 
+    "rural"
+  } else if (appear["midurban"] == "true" && appear["suburban"] == "true"&& appear["urban"] == "true") {
+    "urban"
+  } else if (appear["midurban"] == "true" && appear["suburban"] == "true") {
+     "suburban"
+  } else {
+    "midurban"
+  }
+}
+appearsProcessed <- appears
+appearsProcessed$urbanization <- apply(appears[,urbanColNames], 1, defineUrban)
+appearsProcessed[,urbanColNames] <- NULL
